feb2683727ead3bed1ffd4ef78ff96c2e928d882
Kit Sunde <kitsunde@gmail.com>
2012-09-18 07:13:30 +0800

    Don't use CONCAT when the cb is sqlite3 since it's not supported.

--- friendface/middleware.py
+++ friendface/middleware.py
@@ -1,4 +1,5 @@
 from friendface.models import FacebookApplication
+from django.db.utils import DatabaseError
 
 class FacebookApplicationMiddleware(object):
     def process_request(self, request):
@@ -10,10 +11,26 @@ class FacebookApplicationMiddleware(object):
         current_url = request.build_absolute_uri()
         field = 'secure_canvas_url' if request.is_secure() else 'canvas_url'
 
-        apps = FacebookApplication.objects.extra(
-            select={'{0}_length'.format(field): 'Length({0})'.format(field)},
-            where=["%s LIKE CONCAT({0}, %s)".format(field)],
-            params=[current_url, '%']).order_by('-{0}_length'.format(field))
+        base_extra_args = {
+            'select': {'{0}_length'.format(field): 'Length({0})'.format(field)},
+            'params': [current_url, '%']
+        }
+
+        apps = FacebookApplication.objects.order_by('-{0}_length'.format(field))
+
+        try:
+            extra_args = base_extra_args.copy()
+            extra_args['where'] = ["%s LIKE CONCAT({0}, %s)".format(field)]
+            apps = list(apps.extra(**extra_args))
+        except DatabaseError as e:
+            # SQLite3 cannot do CONCAT so we'll manually filter the list.
+            # Is there a way to dect the datbase backend so we don't have to
+            # try to connect and then recover?
+            if e.message != 'no such function: CONCAT':
+                raise
+
+            apps = filter(lambda a: getattr(a, field).startswith(field),
+                          apps.extra(**base_extra_args))
 
         try:
             setattr(request, 'facebook', apps[0])
