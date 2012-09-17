9f66427fdecc702dac5ba2f0ec9ad272588da69d
Kit Sunde <kitsunde@gmail.com>
2012-09-18 01:33:00 +0800

    Support http and https mapping to different fields on application lookup.

--- friendface/middleware.py
+++ friendface/middleware.py
@@ -2,18 +2,19 @@ from friendface.models import FacebookApplication
 
 class FacebookApplicationMiddleware(object):
     def process_request(self, request):
-        current_url = request.build_absolute_uri()
-        match_url = "^" + current_url.replace('http://', 'https://')
         # This is admittedly a rather strange looking.. lookup.
         # What we need to do is find out if the applications canvas_url is the
         # start of the URL we are currently on. Once that has been established
         # we also need to find out which canvas_url is longer to find out
         # which is the closer match.
+        current_url = request.build_absolute_uri()
+        field = 'secure_canvas_url' if request.is_secure() else 'canvas_url'
 
         apps = FacebookApplication.objects.extra(
-            select={'canvas_url_length': 'Length(canvas_url)'},
-            where=["%s LIKE CONCAT(canvas_url, %s)"],
-            params=[match_url,'%']).order_by('-canvas_url_length')
+            select={'{0}_length'.format(field): 'Length({0})'.format(field)},
+            where=["%s LIKE CONCAT({0}, %s)".format(field)],
+            params=[current_url, '%']).order_by('-{0}_length'.format(field))
+
         try:
             setattr(request, 'facebook', apps[0])
         except IndexError:
