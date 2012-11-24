daca232da08b1bfc3c111d4091ed16ae501733cb
Kit Sunde <kitsunde@gmail.com>
2012-11-25 06:54:16 +0800

    Refactor the FacebookApplication middleware to use the classmethod for looking up the app and add tests for matching.

--- friendface/middleware.py
+++ friendface/middleware.py
@@ -1,3 +1,4 @@
+from urlparse import urlparse
 from django.contrib.auth import authenticate, login
 from django.db import connection
 import re
@@ -8,95 +9,13 @@ class P3PMiddleware(object):
         response['P3P'] = "Nonsense https://support.google.com/accounts/bin/answer.py?hl=en&answer=151657"
         return response
 
+
 class FacebookApplicationMiddleware(object):
-    _match_non_ansi_concat = re.compile('CONCAT\((.+),(.+)\)')
-
-    def _find_applications(self, request):
-        # This rather awkward query finds application by finding all matching
-        # applications and then selecting the ones with the longest match
-        # Attempting to turn this into a single query with the ORM is left as a
-        # futile exercise to the reader
-        current_url = request.build_absolute_uri()
-        canvas = 'secure_canvas_url' if request.is_secure() else 'canvas_url'
-        page_tab = 'secure_page_tab_url' if request.is_secure() else 'page_tab_url'
-
-        # We don't need to query for  website_url or mobile_web_url if there's
-        # a signed_request present
-        if request.POST.get('signed_request'):
-            additional_lookups = """
-                UNION ALL
-                SELECT *, LENGTH(website_url) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT(website_url, %s)
-                UNION ALL
-                SELECT *, LENGTH(mobile_web_url) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT(website_url, %s)"""
-            additional_length_match = """
-                UNION ALL
-                SELECT LENGTH(website_url) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT(website_url, %s)
-                UNION ALL
-                SELECT LENGTH(mobile_web_url) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT(website_url, %s)"""
-        else:
-            additional_lookups = additional_length_match = ''
-
-        query = """
-            SELECT * FROM (
-              SELECT *, LENGTH({page_tab_field}) AS MatchLen
-              FROM friendface_facebookapplication
-              WHERE %s LIKE CONCAT({page_tab_field}, %s)
-              UNION ALL
-              SELECT *, LENGTH({canvas_field}) AS MatchLen
-              FROM friendface_facebookapplication
-              WHERE %s LIKE CONCAT({canvas_field}, %s)
-              {additional_lookups}
-            ) AS matches
-            WHERE MatchLen = (
-              -- Find the maximum length.
-              SELECT MAX(MatchLen) FROM (
-                SELECT LENGTH({page_tab_field}) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT({page_tab_field}, %s)
-                UNION ALL
-                SELECT LENGTH({canvas_field}) AS MatchLen
-                FROM friendface_facebookapplication
-                WHERE %s LIKE CONCAT({canvas_field}, %s)
-                {additional_length_match}
-              ) as LengthMatch);
-        """.format(additional_lookups=additional_lookups,
-                   additional_length_match=additional_length_match,
-                   canvas_field=canvas,
-                   page_tab_field=page_tab)
-
-        # MySQL doesn't support ANSI SQL concat via || without setting
-        # PIPES_AS_CONCAT. Can we detect the PIPES_AS_CONCAT setting?
-        # See: http://dev.mysql.com/doc/refman/5.6/en/ansi-mode.html
-        if not connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
-            query = self._match_non_ansi_concat.sub("\\1 || \\2", query)
-
-        # The Python SQLite adapter doesn't allow %(named_param)s for raw
-        # queries, so to work around that just use %s and replace the old
-        # fashioned way.
-        parameters = [current_url, '%', ] * 4
-        if additional_lookups and additional_length_match:
-            parameters = parameters + ([current_url, '%'] * 4)
-
-        return FacebookApplication.objects.raw(query, parameters)
-
-
-
     def process_request(self, request):
-        # We discover the FacebookApplication relevant to our URL by url
-        # matching the applications settings.
-        applications = self._find_applications(request)
-
         try:
-            setattr(request, 'facebook', applications[0])
-        except IndexError:
+            application = FacebookApplication.get_for_request(request)
+            setattr(request, 'facebook', application)
+        except FacebookApplication.DoesNotExist:
             pass
 
 
