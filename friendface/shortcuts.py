e4b40117112124ebc2a073f716d4dff580e6c02b
Kit Sunde <kitsunde@gmail.com>
2013-01-22 14:57:46 +0800

    Unecessary import.

--- friendface/shortcuts.py
+++ friendface/shortcuts.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.utils.safestring import mark_safe
 
 def redirectjs(redirect_to):
     """Javascript redirect as regular redirects don't affect the top frame."""
