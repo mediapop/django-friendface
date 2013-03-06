17e5fe0a05254c67079ad1523dda03b3d05bfa2f
yarbelk <james.rivettcarnac@gmail.com>
2013-03-06 14:13:02 +0800

    urls now url objects

--- friendface/urls.py
+++ friendface/urls.py
@@ -2,8 +2,8 @@
 from django.conf.urls import *
 
 urlpatterns = patterns('friendface.views',
-    (r'^application/(?P<application_id>\d+)/authorize/', 'authorize'),
-    (r'^authorization/(?P<authorization_id>\d+)/authorized/$', 'authorized'),
-    (r'^record-invitation/$', 'record_facebook_invitation'),
-    (r'^channel/$', 'channel'),
+    url(r'^application/(?P<application_id>\d+)/authorize/', 'authorize', name="authorize"),
+    url(r'^authorization/(?P<authorization_id>\d+)/authorized/$', 'authorized', name="authorized"),
+    url(r'^record-invitation/$', 'record_facebook_invitation', name="record_facebook_invitation"),
+    url(r'^channel/$', 'channel',name="channel"),
 )
