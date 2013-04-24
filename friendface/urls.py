3d8b2d8c3266b800991eb75148de1b646e3245c8
Kit Sunde <kitsunde@gmail.com>
2013-04-24 16:29:48 +0800

    Formatting

--- friendface/urls.py
+++ friendface/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('friendface.views',
     url(r'^application/(?P<application_id>\d+)/authorize/', 'authorize', name="authorize"),
     url(r'^authorization/(?P<authorization_id>\d+)/authorized/$', 'authorized', name="authorized"),
     url(r'^record-invitation/$', 'record_facebook_invitation', name="record_facebook_invitation"),
-    url(r'^channel/$', 'channel',name="channel"),
+    url(r'^channel/$', 'channel', name="channel"),
 )
