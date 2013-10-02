30f19f33888aec9235834c83f4e0e1b2d7950113
Kit Sunde <kitsunde@gmail.com>
2013-10-02 15:55:05 +0800

    Move tests to use django-discover-runner and remove the frontend setup that wasn't testing anything.

---
+++ friendface/tests/settings.py
@@ -0,0 +1,53 @@
+import os
+
+SITE_ID = 1
+
+APP_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+AUTHENTICATION_BACKENDS = (
+    'friendface.auth.backends.FacebookBackend',
+    'django.contrib.auth.backends.ModelBackend'
+)
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+
+    'friendface.tests.accounts',
+    'friendface',
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'friendface.middleware.FacebookApplicationMiddleware',
+    'friendface.middleware.FacebookDecodingMiddleware',
+    'friendface.middleware.DisableCsrfProtectionOnDecodedSignedRequest',
+    'friendface.middleware.FacebookSignedRequestAuthenticationMiddleware',
+)
+
+SECRET_KEY = 'foobar'
+
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+TEMPLATE_DIRS = (os.path.join(APP_ROOT, 'templates'),)
+
+ROOT_URLCONF = 'friendface.tests.urls'
+
+AUTH_PROFILE_MODULE = 'accounts.UserProfile'
