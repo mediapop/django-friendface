85ca7e9a0bf92cad757731f86999d830ff72a92c
Björn Andersson <ba@sanitarium.se>
2014-03-11 00:23:40 +0800

    Use faster password hasher for speeding up tests

--- friendface/tests/settings.py
+++ friendface/tests/settings.py
@@ -51,3 +51,12 @@ ROOT_URLCONF = 'friendface.tests.urls'
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = os.environ.get('AUTH_USER_MODEL', 'auth.User')
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
